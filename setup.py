9120df414695708f8e3bdcca0f2120d723ef6fff
Jean Schurger <jean@schurger.org>
2021-04-01 11:46:07 -0400

    Bumped version.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ ciqw-install-fonts-and-devices = ciqw:install_fonts_and_devices
 """
 
 setup(name='ciqw',
-      version='0.4.4',
+      version='0.4.5',
       description="Connect IQ Wrapper",
       author="Jean Schurger",
       author_email='jean@schurger.org',
